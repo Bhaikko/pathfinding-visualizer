344517df8ac1b540b6cf45d15b2f03cc7d254b70
Siddharth <siddhartha.pawar17@gmail.com>
2021-07-28 22:16:24 +0530

    Path coloring function added

--- src/grid.js
+++ src/grid.js
@@ -7,34 +7,72 @@ const nodeHeight = 20;
 
 const colCount = Math.floor(graphWidth / nodeWidth);
 const rowCount = Math.floor(graphHeight / nodeHeight);
+// const colCount = 4;
+// const rowCount = 4;
 
-class Node {
-    constructor(xCoord, yCoord, htmlRef) {
+class Coordinate {
+    constructor(xCoord, yCoord) {
         this.xCoord = xCoord;
         this.yCoord = yCoord;
-        this.htmlRef = htmlRef;
     }
 }
 
+class Node {
+    constructor(xCoord, yCoord, htmlRef) {
+        this.coordinates = new Coordinate(xCoord, yCoord)
+        this.htmlRef = htmlRef;
+        this.isStart = false;
+        this.isEnd = false;
+        this.isBlocked = false;
+    }
+}
+
+
 const graph = [];
 
-const getIndexByRowCol = (row, col) => (row * rowCount + col)
+const endCoord = new Coordinate(
+    Math.floor(colCount - 1),
+    Math.floor(rowCount - 1)
+);
 
-let graphString = "";
-for (let i = 1; i <= rowCount; i++) {
-    for (let j = 1; j <= colCount; j++) {
+console.log(endCoord);
+
+const getIndexByRowCol = (coord) => (coord.yCoord * rowCount + coord.xCoord)
+
+for (let i = 0; i < rowCount; i++) {
+    for (let j = 0; j < colCount; j++) {
         let graphNode = document.createElement("span");
         graphNode.className = "graph-node";
 
         graph.push(new Node(i, j, graphNode));
 
         graphDiv.appendChild(graphNode);
-        // graphString += `<span class="graph-node"></span>`;
     }
-
-    // graphString += `<br/>`;
 }
 
-// graphDiv.innerHTML = graphString;
+const endNodeIndex = getIndexByRowCol(endCoord);
+const endNode = graph[getIndexByRowCol(endCoord)];
+console.log(endNodeIndex);
 
-console.log(graph[getIndexByRowCol(1, 1)])
+endNode.htmlRef.classList.add("graph-node-end");
+
+// Accessing Node of Graph
+// graph[getIndexByRowCol(0, 0)].htmlRef.setAttribute("class", "hello");
+
+
+const paintPath = pathCoords => {
+    graph.map(node => {
+        pathCoords.map(coord => {
+            if (
+                node.xCoord == coord[0] && 
+                node.yCoord == coord[1]
+            ) {
+                node.htmlRef.classList.add("graph-node-green");
+            }
+        })
+    })
+}
+
+
+
+

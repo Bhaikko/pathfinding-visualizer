d3c90074f45c708af7a9e376ce0698cb17d54a5b
Siddharth <siddhartha.pawar17@gmail.com>
2021-08-04 23:08:25 +0530

    DFS Refactored

--- src/dfs.js
+++ src/dfs.js
@@ -1,114 +1,222 @@
-// Returns Path Array
-const GetPathStack = (endNode, pathStack) => {
-    
-    let currentNode = endNode;
-    
-    while (currentNode != null) {
-        pathStack.Push(currentNode);
-        currentNode = currentNode.parent;
+class DFS {
+    constructor() {
+        this.pathFound = false;
+        
+        this.pathNodes = new Stack();
+        this.visitedNodes = [];
+    }   
+
+    // Returns Path Array
+    GetPathStack = endNode => {
+        let currentNode = endNode;
+        
+        while (currentNode != null) {
+            this.pathNodes.Push(currentNode);
+            currentNode = currentNode.parent;
+        }
+
+        this.pathNodes.stack.reverse();
     }
 
-    pathStack.stack.reverse();
+
+    Dfs = (currentNode, endNode) => {
+        if (
+            currentNode.coordinates.x === endNode.coordinates.x &&
+            currentNode.coordinates.y === endNode.coordinates.y
+        ) {
+            // console.log(currentNode.coordinates, endNode.coordinates)
+    
+            // Path Found
+            this.pathFound = true;
+            this.GetPathStack(endNode);
+            return;
+        }
+    
+        if (currentNode.isVisited || currentNode.isBlocked) {
+            return;
+        }
+        
+        currentNode.isVisited = true;
+        this.visitedNodes.push(currentNode);
+    
+        if (currentNode.coordinates.x + 1 < colCount) {
+            let neighborIndex = getIndexByRowCol(new Coordinate(
+                currentNode.coordinates.x + 1, 
+                currentNode.coordinates.y
+            ));
+    
+            let neighborNode = graph[neighborIndex];
+    
+            if (!neighborNode.isVisited) {
+                neighborNode.parent = currentNode;
+        
+                this.Dfs(neighborNode, endNode);
+    
+                if (this.pathFound) {
+                    return;
+                }
+            }
+    
+        }
+    
+        if (currentNode.coordinates.y + 1 < rowCount) {
+            let neighborIndex = getIndexByRowCol(new Coordinate(
+                currentNode.coordinates.x, 
+                currentNode.coordinates.y + 1
+            ));
+    
+            let neighborNode = graph[neighborIndex];
+            
+            if (!neighborNode.isVisited) {
+                neighborNode.parent = currentNode;
+        
+                this.Dfs(neighborNode, endNode);
+    
+                if (this.pathFound) {
+                    return;
+                }
+            }
+        }
+    
+        if (currentNode.coordinates.x - 1 >= 0) {
+            let neighborIndex = getIndexByRowCol(new Coordinate(
+                currentNode.coordinates.x - 1, 
+                currentNode.coordinates.y
+            ));
+    
+            let neighborNode = graph[neighborIndex];
+            
+            if (!neighborNode.isVisited) {
+                neighborNode.parent = currentNode;
+        
+                this.Dfs(neighborNode, endNode);
+    
+                if (this.pathFound) {
+                    return;
+                }
+            }
+        }
+    
+        if (currentNode.coordinates.y - 1 >= 0) {
+            let neighborIndex = getIndexByRowCol(new Coordinate(
+                currentNode.coordinates.x, 
+                currentNode.coordinates.y - 1
+            ));
+    
+            let neighborNode = graph[neighborIndex];
+           
+            if (!neighborNode.isVisited) {
+                neighborNode.parent = currentNode;
+        
+                this.Dfs(neighborNode, endNode);
+    
+                if (this.pathFound) {
+                    return;
+                }
+            }
+        }
+    }
 }
 
-let pathFound = false;
 
 
-const DFS = (currentNode, endNode, pathStack, nodesVisited) => {
-    if (
-        currentNode.coordinates.x === endNode.coordinates.x &&
-        currentNode.coordinates.y === endNode.coordinates.y
-    ) {
-        // console.log(currentNode.coordinates, endNode.coordinates)
 
-        // Path Found
-        pathFound = true;
-        GetPathStack(endNode, pathStack);
-        return;
-    }
+// const DFS = (currentNode, endNode, pathStack, nodesVisited) => {
+//     if (
+//         currentNode.coordinates.x === endNode.coordinates.x &&
+//         currentNode.coordinates.y === endNode.coordinates.y
+//     ) {
+//         // console.log(currentNode.coordinates, endNode.coordinates)
 
-    if (currentNode.isVisited || currentNode.isBlocked) {
-        return;
-    }
+//         // Path Found
+//         pathFound = true;
+//         GetPathStack(endNode, pathStack);
+//         return;
+//     }
+
+//     if (currentNode.isVisited || currentNode.isBlocked) {
+//         return;
+//     }
     
-    currentNode.isVisited = true;
-    nodesVisited.push(currentNode);
+//     currentNode.isVisited = true;
+//     nodesVisited.push(currentNode);
 
-    if (currentNode.coordinates.x + 1 < colCount) {
-        let neighborIndex = getIndexByRowCol(new Coordinate(
-            currentNode.coordinates.x + 1, 
-            currentNode.coordinates.y
-        ));
+//     if (currentNode.coordinates.x + 1 < colCount) {
+//         let neighborIndex = getIndexByRowCol(new Coordinate(
+//             currentNode.coordinates.x + 1, 
+//             currentNode.coordinates.y
+//         ));
 
-        let neighborNode = graph[neighborIndex];
+//         let neighborNode = graph[neighborIndex];
 
-        if (!neighborNode.isVisited) {
-            neighborNode.parent = currentNode;
+//         if (!neighborNode.isVisited) {
+//             neighborNode.parent = currentNode;
     
-            DFS(neighborNode, endNode, pathStack, nodesVisited);
+//             DFS(neighborNode, endNode, pathStack, nodesVisited);
 
-            if (pathFound) {
-                return;
-            }
-        }
+//             if (pathFound) {
+//                 return;
+//             }
+//         }
 
-    }
+//     }
 
-    if (currentNode.coordinates.y + 1 < rowCount) {
-        let neighborIndex = getIndexByRowCol(new Coordinate(
-            currentNode.coordinates.x, 
-            currentNode.coordinates.y + 1
-        ));
+//     if (currentNode.coordinates.y + 1 < rowCount) {
+//         let neighborIndex = getIndexByRowCol(new Coordinate(
+//             currentNode.coordinates.x, 
+//             currentNode.coordinates.y + 1
+//         ));
 
-        let neighborNode = graph[neighborIndex];
+//         let neighborNode = graph[neighborIndex];
         
-        if (!neighborNode.isVisited) {
-            neighborNode.parent = currentNode;
+//         if (!neighborNode.isVisited) {
+//             neighborNode.parent = currentNode;
     
-            DFS(neighborNode, endNode, pathStack, nodesVisited);
+//             DFS(neighborNode, endNode, pathStack, nodesVisited);
 
-            if (pathFound) {
-                return;
-            }
-        }
-    }
+//             if (pathFound) {
+//                 return;
+//             }
+//         }
+//     }
 
-    if (currentNode.coordinates.x - 1 >= 0) {
-        let neighborIndex = getIndexByRowCol(new Coordinate(
-            currentNode.coordinates.x - 1, 
-            currentNode.coordinates.y
-        ));
+//     if (currentNode.coordinates.x - 1 >= 0) {
+//         let neighborIndex = getIndexByRowCol(new Coordinate(
+//             currentNode.coordinates.x - 1, 
+//             currentNode.coordinates.y
+//         ));
 
-        let neighborNode = graph[neighborIndex];
+//         let neighborNode = graph[neighborIndex];
         
-        if (!neighborNode.isVisited) {
-            neighborNode.parent = currentNode;
+//         if (!neighborNode.isVisited) {
+//             neighborNode.parent = currentNode;
     
-            DFS(neighborNode, endNode, pathStack, nodesVisited);
+//             DFS(neighborNode, endNode, pathStack, nodesVisited);
 
-            if (pathFound) {
-                return;
-            }
-        }
-    }
+//             if (pathFound) {
+//                 return;
+//             }
+//         }
+//     }
 
-    if (currentNode.coordinates.y - 1 >= 0) {
-        let neighborIndex = getIndexByRowCol(new Coordinate(
-            currentNode.coordinates.x, 
-            currentNode.coordinates.y - 1
-        ));
+//     if (currentNode.coordinates.y - 1 >= 0) {
+//         let neighborIndex = getIndexByRowCol(new Coordinate(
+//             currentNode.coordinates.x, 
+//             currentNode.coordinates.y - 1
+//         ));
 
-        let neighborNode = graph[neighborIndex];
+//         let neighborNode = graph[neighborIndex];
        
-        if (!neighborNode.isVisited) {
-            neighborNode.parent = currentNode;
+//         if (!neighborNode.isVisited) {
+//             neighborNode.parent = currentNode;
     
-            DFS(neighborNode, endNode, pathStack, nodesVisited);
+//             DFS(neighborNode, endNode, pathStack, nodesVisited);
 
-            if (pathFound) {
-                return;
-            }
-        }
-    }
+//             if (pathFound) {
+//                 return;
+//             }
+//         }
+//     }
 
-}
+// }
+
--- src/ui.js
+++ src/ui.js
@@ -1,19 +1,22 @@
 const visualizeBtn = document.getElementById("visualize-btn");
 
 visualizeBtn.addEventListener("click", event => {
-    let pathStack = new Stack();
-    let nodesVisited = [];
+    // let pathStack = new Stack();
+    // let nodesVisited = [];
 
-    DFS(startNode, endNode, pathStack, nodesVisited);
+    const dfs = new DFS();
+    dfs.Dfs(startNode, endNode);
 
-    if (pathStack.stack.length === 0) {
+    // DFS(startNode, endNode, pathStack, nodesVisited);
+
+    if (dfs.pathNodes.stack.length === 0) {
         console.log("No Path Found");
     } else {
         // console.log(pathStack.stack);
-        paintPath(nodesVisited, "graph-node-red");
+        paintPath(dfs.visitedNodes, "graph-node-red");
 
         setTimeout(() => {
-            paintPath(pathStack.stack, "graph-node-green");
+            paintPath(dfs.pathNodes.stack, "graph-node-green");
         }, 1000)
     }
 });
